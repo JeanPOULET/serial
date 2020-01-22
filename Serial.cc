3af62d527e29d788da7d0ab6aaa17359599ab8e4
MathieuBart <mathieu.bart3@gmail.com>
2020-01-22 15:43:36 +0100

    c'est chiant

--- Serial.cc
+++ Serial.cc
@@ -289,10 +289,14 @@ namespace serial {
     IBinaryFile& operator>>(IBinaryFile& file, std::string& x){
         int32_t lg;
         std::string st;
+        
         file >>lg;
         for(int32_t i=0; i<lg;++i){
-            file>>st;
+            char c;
+            file>>c;
+            st.push_back(c);
         }
+        x=st;
         return file;
     }
 
